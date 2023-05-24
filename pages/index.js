e9bffa9e1fc312fa3db3d009967a4b84a0c296e8
Junkyu Park <junqwoopark@gmail.com>
2023-05-24 16:14:51 +0900

    study: Server Side Rendering, getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,12 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-export default function Home() {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({ results }) {
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies ? <h4>Loading...</h4> : null}
-      {movies?.map((movie) => (
+
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -42,4 +35,15 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:3000/api/movies");
+  const { results } = await res.json();
+
+  return {
+    props: {
+      results,
+    },
+  };
 }
