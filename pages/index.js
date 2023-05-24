5c9d9e9cf943a634f9f0e0354885ea9364945f1f
Junkyu Park <junqwoopark@gmail.com>
2023-05-24 12:28:54 +0900

    study: framework, pages, static pre-rendering

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,19 @@
-export default function Potato() {
-  return "hi";
+// pages 폴더 안에 있는 파일명에 따라 라우팅이 됨.
+// index.js는 기본적으로 라우팅이 되는 파일임.
+// 즉, localhost:3000/ 으로 접속하면 index.js가 렌더링 됨.
+
+import { useState } from "react";
+
+// 파일 내 함수명은 반드시 export default로 선언해야 함.
+// 그렇지 않으면 라우팅이 되지 않음.
+
+// Static Pre-rendering을 지원함.
+export default function Home() {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div>
+      <h1>Hello, {counter}</h1>
+      <button onClick={() => setCounter((prev) => prev + 1)}>Button</button>
+    </div>
+  );
 }
